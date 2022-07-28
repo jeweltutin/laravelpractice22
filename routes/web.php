3322672d2b40bbf87ab2ebfe6b7ba3662002ff5a
jeweltutin <jeweltutin@gmail.com>
2022-07-28 17:58:09 +0600

    simple admin layout page ready bootstrap 3

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/datatable', function(){
 	return view('datatable');
 });
+
+Route::get('/bootstarp', function(){
+    return view('/bootstrapview');
+});
